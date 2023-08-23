8ff1d7b50f7a252a3aa5f229cb1b2211706eab7f
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-23 15:51:32 +0200

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -4,4 +4,13 @@ from time import sleep
 from datetime import datetime
 
 def getGoldPrice():
-  url = ' '
+  url = 'https://www.gold.org/'
+  req = urllib.request.urlopen(url)
+  page = req.read()
+  scraping = BeatifulSoup(page)
+  price = scraping.findAll('td', attrs = {'id': spotpriceCellAsk'})[0]
+                                          .text
+                                          return price
+
+
+                                        
