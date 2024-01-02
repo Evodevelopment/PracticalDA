25a5132dea397ca06d3c51e6b517e98f87aa65f1
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2024-01-02 18:00:47 +0100

    Create csvReaderNumpy.py

---
+++ Chapter2/csvReaderNumpy.py
@@ -0,0 +1,7 @@
+import numpy
+
+data = numpy.genfromtxt("pokemon.csv"
+                        ,skip_header=1
+                        ,dtype=None
+                        ,delimiter=',')
+print(data)
