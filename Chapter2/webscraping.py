9b783b2473ded8b5ec4b8da8918a115b0d8b88aa
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2024-01-02 18:03:45 +0100

    Update and rename webscraping.py to Chapter2/webscraping.py

--- Chapter2/webscraping.py
+++ Chapter2/webscraping.py
@@ -18,7 +18,7 @@ with open("goldPrice.out","w") as f:
         f.write("{0}, {1} \n ".format(sNow, getGoldPrice()))
         sleep(1)
 
-#Beautiful Soup is a Python library for pulling data out of HTML and XML files. It helps you to get the data in fun, condensed way.
+#Beautiful Soup is a Python library for pulling data out of HTML and XML files. 
 # It works with your favorite parser to provide idiomatic 
 # ways of navigating, searching, and modifying the parse tree. It commonly saves programmers hours or days of work.
 
