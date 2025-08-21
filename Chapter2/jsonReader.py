b1e17f3cddb16f5fcc52fdbb4dd62c5ed008fecb
JDrepos <66202511+Evodevelopment@users.noreply.github.com>
2025-08-21 20:11:02 +0200

    Update jsonReader.py

--- Chapter2/jsonReader.py
+++ Chapter2/jsonReader.py
@@ -4,3 +4,28 @@ from pprint import pprint
 with open("pokemon.json") as f:
     data = json.loads(f.read())
     pprint(data)
+
+Edit json:
+{
+  "id": 25,
+  "name": "Pikachu",
+  "type": [
+    "Electric"
+  ],
+  "isLegendary": false,
+  "stats": {
+    "hp": 35,
+    "attack": 55,
+    "defense": 40
+  },
+  "moves": [
+    {
+      "name": "Thunder Shock",
+      "power": 40
+    },
+    {
+      "name": "Quick Attack",
+      "power": 40
+    }
+  ]
+}
